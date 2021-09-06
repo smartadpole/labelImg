82c1e742b97241d048d130af9148acc404beeccc
liujiaxing7 <1445101363@qq.com>
2021-09-06 18:32:21 +0800

    Multi-model detection

--- libs/detector/yolov3/postprocess/postprocess.py
+++ libs/detector/yolov3/postprocess/postprocess.py
@@ -107,48 +107,48 @@ def nms_cpu(boxes, confs, nms_thresh=0.5, min_mode=False):
 
     return np.array(keep)
 
-def weighted_nms(dets, thresh=0.5):
-    """
-    Takes bounding boxes and scores and a threshold and applies
-    weighted non-maximal suppression.
-    """
-    scores = dets[:,4]
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-
-    areas = (x2 - x1) * (y2 - y1)
-    order = scores.argsort()[::-1]
-
-    max_ids = []
-    weighted_boxes = []
-    while order.size > 0:
-        i = order[0]
-        max_ids.append(i)
-        xx1 = np.maximum(x1[i], x1[order[:]])
-        yy1 = np.maximum(y1[i], y1[order[:]])
-        xx2 = np.minimum(x2[i], x2[order[:]])
-        yy2 = np.minimum(y2[i], y2[order[:]])
-
-        w = np.maximum(0.0, xx2 - xx1)
-        h = np.maximum(0.0, yy2 - yy1)
-        inter = w * h
-        iou = inter / (areas[i] + areas[order[:]] - inter)
-
-        in_inds = np.where(iou >= thresh)[0]
-        in_dets = dets[in_inds, :]
-
-        weights = in_dets[:, 4] * iou[in_inds]
-        wbox = np.sum((in_dets[:, :4] * weights[..., np.newaxis]), axis=0) \
-            / np.sum(weights)
-        weighted_boxes.append(wbox)
-
-        out_inds = np.where(iou < thresh)[0]
-        order = order[out_inds]
-        dets = dets[out_inds]
-
-    return max_ids, np.array(weighted_boxes)
+# def weighted_nms(dets, thresh=0.5):
+#     """
+#     Takes bounding boxes and scores and a threshold and applies
+#     weighted non-maximal suppression.
+#     """
+#     scores = dets[:,4]
+#     x1 = dets[:, 0]
+#     y1 = dets[:, 1]
+#     x2 = dets[:, 2]
+#     y2 = dets[:, 3]
+#
+#     areas = (x2 - x1) * (y2 - y1)
+#     order = scores.argsort()[::-1]
+#
+#     max_ids = []
+#     weighted_boxes = []
+#     while order.size > 0:
+#         i = order[0]
+#         max_ids.append(i)
+#         xx1 = np.maximum(x1[i], x1[order[:]])
+#         yy1 = np.maximum(y1[i], y1[order[:]])
+#         xx2 = np.minimum(x2[i], x2[order[:]])
+#         yy2 = np.minimum(y2[i], y2[order[:]])
+#
+#         w = np.maximum(0.0, xx2 - xx1)
+#         h = np.maximum(0.0, yy2 - yy1)
+#         inter = w * h
+#         iou = inter / (areas[i] + areas[order[:]] - inter)
+#
+#         in_inds = np.where(iou >= thresh)[0]
+#         in_dets = dets[in_inds, :]
+#
+#         weights = in_dets[:, 4] * iou[in_inds]
+#         wbox = np.sum((in_dets[:, :4] * weights[..., np.newaxis]), axis=0) \
+#             / np.sum(weights)
+#         weighted_boxes.append(wbox)
+#
+#         out_inds = np.where(iou < thresh)[0]
+#         order = order[out_inds]
+#         dets = dets[out_inds]
+#
+#     return max_ids, np.array(weighted_boxes)
 
 
 def bbox_iou1(box1, box2, x1y1x2y2=True):
